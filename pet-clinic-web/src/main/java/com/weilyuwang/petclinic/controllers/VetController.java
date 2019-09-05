525ba1df93948011c40e889a1e92fb10bfe8194e
Weilyu Wang <weilyuwang@gmail.com>
2019-09-05 17:24:15 -0400

    List all Vets on vet index page. closes #15

--- pet-clinic-web/src/main/java/com/weilyuwang/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/weilyuwang/petclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.weilyuwang.petclinic.controllers;
 
+import com.weilyuwang.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
