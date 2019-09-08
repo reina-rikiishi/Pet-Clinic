1e822089e88ad6ca27d396255c8ec259975a14f2
Weilyu Wang <weilyuwang@gmail.com>
2019-09-07 21:49:41 -0400

    Implement Base Entity (JPA). closes #35

--- pet-clinic-data/src/main/java/com/weilyuwang/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/weilyuwang/petclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.weilyuwang.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@MappedSuperclass //this is saying "hey JPA, we're going to inherit from this class or other classes are going to inheriting it"
 public class BaseEntity implements Serializable {
 
+    @Id //this annotation tells JPA that this is the ID value
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //prefer boxed types to primitives(long) for hibernate, in that boxed types can be Null whereas primitive types cannot.
 
     public Long getId() {
